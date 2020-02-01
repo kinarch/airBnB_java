1366aa34f934a1ed6292e5a4d34bffc99789a1dc
richard <rai_jinn@pm.me>
2020-02-01 13:52:48 +0100

    quelques améliorations

--- src/richard/airbnb/menu/Menu.java
+++ src/richard/airbnb/menu/Menu.java
@@ -22,6 +22,7 @@ public class Menu {
     }
 
     public static void main(String[] args) throws Exception {
+
         System.out.println("Bienvenue chez AirBnB");
 
         scanner = new Scanner(System.in);
@@ -31,7 +32,7 @@ public class Menu {
         listeHotes.add(new Hote("Albert", "Einstein", 76, 48));
         listeHotes.add(new Hote("Friedrich", "Nietzsche", 55, 12));
         listeLogements.add(new Maison(listeHotes.get(0), "Quelque part", 40, 200, 5, 200, true));
-        listeLogements.add(new Appartement(listeHotes.get(1), "Monde imaginaire", 80, 150, 20, 5, 50));
+        listeLogements.add(new Appartement(listeHotes.get(1), "Dans un Monde imaginaire", 80, 150, 20, 5, 50));
         listeVoyageurs.add(new Voyageur("Voyageur", "du Temp", 30));
         listeVoyageurs.add(new Voyageur("Doctor", "Who", 100));
 
